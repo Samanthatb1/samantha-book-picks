633d1928f8b4240b6286e3b3a09b7594c06984bb
Samantha Grieco <samtb1@outlook.com>
2020-07-17 16:53:16 -0400

    added layout for books

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 setInterval(setClock, 1000)
 
 const hourHand = document.querySelector('[data-hour-hand]')
@@ -7,19 +6,17 @@ const secondHand = document.querySelector('[data-second-hand]')
 
 function setClock() {
   var currentDate = new Date()
-  currentDate.setMinutes(0)
   var secondsRatio = currentDate.getSeconds() / 60
   var minutesRatio = (secondsRatio + currentDate.getMinutes()) / 61
   var hoursRatio = (minutesRatio + currentDate.getHours()) / 12
   setRotation(secondHand, secondsRatio)
   setRotation(minuteHand, minutesRatio)
   setRotation(hourHand, hoursRatio)
-  console.log(secondsRatio, minutesRatio, hoursRatio)
 }
 
 function setRotation(element, rotationRatio) {
   element.style.setProperty('--rotation', rotationRatio * 360)
-  console.log("--rotation")
 }
 
 setClock()
+
